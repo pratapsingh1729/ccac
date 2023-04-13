a92d9e05395442fdf67a7a96f28c0500cfb3a6f6
Pratap Singh <pratapsingh1729@gmail.com>
2023-04-12 20:08:13 -0400

    cleanup

--- bb_abr_queries.py
+++ bb_abr_queries.py
@@ -4,6 +4,7 @@ from config import ModelConfig
 from model import make_solver
 from my_solver import MySolver
 from variables import Variables
+from plot import plot_model
 
 
 
@@ -11,7 +12,6 @@ from variables import Variables
 
 def run_abr_query(cca, abr, utilization_threshold):
     from cache import run_query
-    from plot import plot_model
     from utils import make_periodic
 
     c = ModelConfig(N=1,
@@ -64,22 +64,19 @@ def run_abr_query(cca, abr, utilization_threshold):
 
     # conds = []
     # for t in range(0, c.T - 1):
-    #     conds.append(
+    #     conds.append(And(
     #             # Cwnd is decreasing in this iteration
-    #             # v.c_f[0][t+1] == v.c_f[0][t],
+    #             v.c_f[0][t+1] < v.c_f[0][t],
     #             # Service in this timestep is low
     #             # v.S[t+1] - v.S[t] <= utilization_threshold * c.C
-    #         )
+    #         ))
     # s.add(Or(*conds))
-    s.add(v.S[-1] - v.S[0] < 0.1 * c.C * (c.T - 0))
+    s.add(v.S[-1] - v.S[0] < utilization_threshold * c.C * (c.T - 0))
 
     make_periodic(c,s,v,dur)
 
     qres = run_query(s, c, timeout=1200)
-    print(qres.satisfiable)
-    if str(qres.satisfiable) == "sat":
-        plot_model(qres.model, c)
-    return qres
+    return qres, c
 
 
 if __name__ == "__main__":
@@ -88,18 +85,20 @@ if __name__ == "__main__":
     #     for cca in ["aimd", "rocc", "copa", "bbr"]:
     #         for abr in ["bulk", "bb_abr"]:
     #             print(f"cca = {cca}, abr = {abr}")
-    #             qres = run_abr_query(cca, abr, utilization_threshold)
-    #             if qres is None:
+    #             result, _ = run_abr_query(cca, abr, utilization_threshold)
+    #             if result is None:
     #                 print("\t\terror\n")
     #             else:
-    #                 print(f"\t\t{qres.satisfiable}")
+    #                 print(f"\t\t{result.satisfiable}")
     for utilization_threshold in [0.1]:
         print(f"\n\n\nutilization_threshold = {utilization_threshold}\n")
         for cca in ["aimd"]:
             for abr in ["bulk", "bb_abr"]:
                 print(f"cca = {cca}, abr = {abr}")
-                result = run_abr_query(cca, abr, utilization_threshold)
+                result, c = run_abr_query(cca, abr, utilization_threshold)
                 if result is None:
                     print("\t\terror\n")
                 else:
                     print(f"\t\t{result.satisfiable}")        
+                    if str(result.satisfiable) == "sat":
+                        plot_model(result.model, c)
