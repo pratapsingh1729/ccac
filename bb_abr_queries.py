60d4773398420ffa46940414366250b553150584
Pratap Singh <pratapsingh1729@gmail.com>
2023-04-13 18:18:32 -0400

    first attempt at a buffering query, needs to be refined

--- bb_abr_queries.py
+++ bb_abr_queries.py
@@ -10,7 +10,7 @@ from plot import plot_model
 
 
 
-def run_abr_query(cca, abr, utilization_threshold):
+def run_abr_utilization_query(cca, abr, utilization_threshold):
     from cache import run_query
     from utils import make_periodic
 
@@ -78,6 +78,69 @@ def run_abr_query(cca, abr, utilization_threshold):
     qres = run_query(s, c, timeout=1200)
     return qres, c
 
+def run_abr_buffering_query(cca, abr, buffering_timesteps):
+    from cache import run_query
+    from utils import make_periodic
+
+    c = ModelConfig(N=1,
+                    D=1,
+                    R=1,
+                    T=10,
+                    C=1,
+                    buf_min=2,
+                    buf_max=None, # infinity
+                    dupacks=None,
+                    cca=cca, # only rocc and aimd are good in the compose=true model, copa in compose=false
+                    compose=False, # whether the model admits multiple network elements (composition property)
+                    alpha=None,
+                    pacing=False,
+                    epsilon="zero",
+                    unsat_core=False,
+                    simplify=False,
+                    app=abr)
+
+    # for AIMD dur=1, for Copa dur=c.R+c.D, for BBR dur=2*c.R
+    # start_T = None
+    dur = None
+    if cca=="aimd":
+        dur = 1
+    elif cca=="rocc":
+        dur = 4
+    elif cca=="copa":
+        dur = c.R+c.D
+    elif cca=="bbr":
+        dur = 2*c.R
+    else:
+        print("Unrecognized CCA")
+        return None
+
+    # Query: Buffering    
+    s, v = make_solver(c)
+
+    # Require a small alpha (i.e. a high link rate in terms of MSS/timestep)
+    s.add(v.alpha <= 0.25 * c.C * c.R)
+    # s.add(v.L[0] == 0)
+
+    # We don't want an example with timeouts
+    for t in range(c.T):
+        s.add(Not(v.timeout_f[0][t]))
+
+    conds = []
+    for t in range(c.T - buffering_timesteps):
+        ands = []
+        for i in range(t, t + buffering_timesteps):
+            ands.append(v.av[0].b[i] == 0)
+        conds.append(And(*ands))
+    s.add(Or(*conds))
+
+    # don't include initial buffering
+    s.add(v.av[0].b[0] != 0)
+
+    make_periodic(c,s,v,dur)
+
+    qres = run_query(s, c, timeout=1200)
+    return qres, c
+
 
 if __name__ == "__main__":
     # for utilization_threshold in [0.1, 0.25, 0.5, 0.6]:
@@ -90,15 +153,27 @@ if __name__ == "__main__":
     #                 print("\t\terror\n")
     #             else:
     #                 print(f"\t\t{result.satisfiable}")
-    for utilization_threshold in [0.1]:
-        print(f"\n\n\nutilization_threshold = {utilization_threshold}\n")
+    # for utilization_threshold in [0.1]:
+    #     print(f"\n\n\nutilization_threshold = {utilization_threshold}\n")
+    #     for cca in ["aimd"]:
+    #         for abr in ["bulk", "bb_abr"]:
+    #             print(f"cca = {cca}, abr = {abr}")
+    #             result, c = run_abr_utilization_query(cca, abr, utilization_threshold)
+    #             if result is None:
+    #                 print("\t\terror\n")
+    #             else:
+    #                 print(f"\t\t{result.satisfiable}")        
+    #                 if str(result.satisfiable) == "sat":
+    #                     plot_model(result.model, c)
+    for buffering_timesteps in [2,3,4,5]:
+        print(f"\n\nbuffering_timesteps = {buffering_timesteps}\n")
         for cca in ["aimd"]:
-            for abr in ["bulk", "bb_abr"]:
+            for abr in ["bb_abr"]:
                 print(f"cca = {cca}, abr = {abr}")
-                result, c = run_abr_query(cca, abr, utilization_threshold)
+                result, c = run_abr_buffering_query(cca, abr, buffering_timesteps)
                 if result is None:
                     print("\t\terror\n")
                 else:
-                    print(f"\t\t{result.satisfiable}")        
+                    print(f"\t\t{result.satisfiable}")
                     if str(result.satisfiable) == "sat":
                         plot_model(result.model, c)
